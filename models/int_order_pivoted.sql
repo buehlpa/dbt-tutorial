546a0514da8582f1bc081291dad6ebf2c78e8ca7
buehlpa <buehler1991@gmail.com>
2026-01-13 10:27:38 +0100

    added loop.last for comma

--- models/int_order_pivoted.sql
+++ models/int_order_pivoted.sql
@@ -9,9 +9,9 @@ pivoted as (
     select
         order_id,
         {% set payment_methods=['bank_transfer','coupon','credit_card','gift_card'] %}
-        {% for p_method in payment_methods %}
+        {%- for p_method in payment_methods -%}
 
-            sum(case when payment_method = '{{p_method}}' then amount else 0 end) as {{p_method}}_amount,
+            sum(case when payment_method = '{{p_method}}' then amount else 0 end) as {{p_method}}_amount {%- if not loop.last -%} , {% endif %}
 
         {% endfor %}
     from payments
